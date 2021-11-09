1bad0f0a76651ac603bccb0ead7760b297f3ddd0
Jason Gayle <jasonblue1626@gmail.com>
2021-11-08 22:20:42 -0500

    Added some comments

--- fork.c
+++ fork.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
-#include<sys/wait.h>
+#include <sys/wait.h>
 
+int main()
+{
 
-int main(){
-
-    int i,j;
-    j=0;
-    pid_t cpid; 
+    // Initializing and declaring values to be used
+    int i, j;
+    j = 0;
+    pid_t cpid;
 
     printf("Ready to fork...\n");
-    printf("Parent pid = %d\n", getpid()); 
-    i=fork();
-    if (i == 0){
+
+    // Retrieve id of process currently running
+    printf("Parent pid = %d\n", getpid());
+
+    // Creates a child process, where the first value will be 0
+    i = fork();
+    if (i == 0)
+    {
         printf("The child executes this code.\n");
-        for (i=0; i<5; i++)
-            j=j+i;
-        printf("Child j=%d\n",j);
-    } else {
-        // cpid = wait(NULL);
+
+        // Loops and prints processes currently running
+        for (i = 0; i < 5; i++)
+            j = j + i;
+        printf("Child j=%d\n", j);
+    }
+    else
+    {
+        cpid = wait(NULL);
         printf("The parent executes this code.\n");
-        for (i=0; i<3; i++)
-                j=j+i;
-        printf("Parent j=%d\n",j);
-        // printf("Child pid = %d\n", cpid); 
+        for (i = 0; i < 3; i++)
+            j = j + i;
+        printf("Parent j=%d\n", j);
+        printf("Child pid = %d\n", cpid);
     }
 }
--- forkv2.c
+++ forkv2.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(){
+int main()
+{
     int i;
 
-    i=getpid();
-    printf("Parent=%d\n",i);
+    i = getpid();
+    printf("Parent=%d\n", i);
+
+    // Create child process
     fork();
+
+    // Create child of child process
     fork();
-    i=getpid();
-    printf("Who am I? %d\n",i);
+
+    // Retrieve process ID for second child process
+    i = getpid();
+    printf("Who am I? %d\n", i);
 
     return 0;
 }
-
